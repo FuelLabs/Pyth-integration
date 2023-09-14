ada67db897e165efd39e722534edb2b5df9bddf2
K1-R1 <k1r1.devwork@gmail.com>
2023-09-14 17:08:59 +0100

    abigen error

---
+++ pyth-contract/tests/harness.rs
@@ -0,0 +1,45 @@
+use fuels::{prelude::*, types::ContractId};
+
+// Load abi from json
+abigen!(Contract(
+    name = "MyContract",
+    abi = "pyth-contract/out/debug/pyth-contract-abi.json"
+));
+
+async fn get_contract_instance() -> (MyContract<WalletUnlocked>, ContractId) {
+    // Launch a local network and deploy the contract
+    let mut wallets = launch_custom_provider_and_get_wallets(
+        WalletsConfig::new(
+            Some(1),             /* Single wallet */
+            Some(1),             /* Single coin (UTXO) */
+            Some(1_000_000_000), /* Amount per coin */
+        ),
+        None,
+        None,
+    )
+    .await;
+    let wallet = wallets.pop().unwrap();
+
+    let storage_config =
+        StorageConfiguration::load_from("pyth-contract/out/debug/pyth-contract-storage_slots.json")
+            .unwrap();
+
+    let load_config = LoadConfiguration::default().with_storage_configuration(storage_config);
+
+    let id = Contract::load_from("pyth-contract/out/debug/pyth-contract.bin", load_config)
+        .unwrap()
+        .deploy(&wallet, TxParameters::default())
+        .await
+        .unwrap();
+
+    let instance = MyContract::new(id.clone(), wallet);
+
+    (instance, id.into())
+}
+
+#[tokio::test]
+async fn can_get_contract_id() {
+    let (_instance, _id) = get_contract_instance().await;
+
+    // Now you have an instance of your contract you can use to test each function
+}
