c80162c0474bfe74bd41969bc00c80a2e98549a1
K1-R1 <k1r1.devwork@gmail.com>
2023-09-19 15:35:23 +0100

    Test: update_fee

---
+++ pyth-contract/tests/functions/pyth_core/update_fee.rs
@@ -0,0 +1,35 @@
+use crate::utils::{
+    interface::{pyth_core::update_fee, pyth_init::constructor},
+    setup::{
+        default_data_sources, default_update_data_bytes, guardian_set_upgrade_3_vaa_bytes,
+        setup_environment, DEFAULT_SINGLE_UPDATE_FEE, DEFAULT_VALID_TIME_PERIOD,
+    },
+};
+use fuels::types::Bytes;
+
+mod success {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn gets_update_fee() {
+        let (_oracle_contract_id, deployer) = setup_environment().await;
+
+        constructor(
+            &deployer.oracle_contract_instance,
+            default_data_sources(),
+            DEFAULT_SINGLE_UPDATE_FEE,
+            DEFAULT_VALID_TIME_PERIOD,
+            Bytes(guardian_set_upgrade_3_vaa_bytes()),
+        )
+        .await;
+
+        let response = update_fee(
+            &deployer.oracle_contract_instance,
+            default_update_data_bytes(),
+        )
+        .await;
+
+        assert_eq!(response.value, default_update_data_bytes().len() as u64);
+    }
+}
--- pyth-contract/tests/utils/interface/pyth_core.rs
+++ pyth-contract/tests/utils/interface/pyth_core.rs
@@ -1,6 +1,20 @@
-use fuels::{accounts::wallet::WalletUnlocked, programs::call_response::FuelCallResponse};
+use fuels::{
+    accounts::wallet::WalletUnlocked, prelude::Bytes, programs::call_response::FuelCallResponse,
+};
 
 use crate::utils::setup::PythOracleContract;
+
+pub(crate) async fn update_fee(
+    contract: &PythOracleContract<WalletUnlocked>,
+    update_data: Vec<Bytes>,
+) -> FuelCallResponse<u64> {
+    contract
+        .methods()
+        .update_fee(update_data)
+        .call()
+        .await
+        .unwrap()
+}
 
 pub(crate) async fn valid_time_period(
     contract: &PythOracleContract<WalletUnlocked>,
