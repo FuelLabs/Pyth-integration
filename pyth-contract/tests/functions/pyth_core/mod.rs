3a2c6b47d9d19809e92adc4557a9732d5b418436
K1-R1 <k1r1.devwork@gmail.com>
2023-09-20 01:26:06 +0100

    test: price

--- pyth-contract/tests/functions/pyth_core/mod.rs
+++ pyth-contract/tests/functions/pyth_core/mod.rs
@@ -1,2 +1,3 @@
+pub(crate) mod price;
 pub(crate) mod update_fee;
 pub(crate) mod update_price_feeds;
---
+++ pyth-contract/tests/functions/pyth_core/price.rs
@@ -0,0 +1,67 @@
+use crate::utils::{
+    interface::{
+        pyth_core::{price, update_fee, update_price_feeds},
+        pyth_init::constructor,
+    },
+    setup::{
+        default_data_sources, default_price_feed_ids, default_update_data_bytes,
+        guardian_set_upgrade_3_vaa_bytes, setup_environment, DEFAULT_SINGLE_UPDATE_FEE,
+        ETH_USD_PRICE, EXTENDED_TIME_PERIOD, USDC_USD_PRICE,
+    },
+};
+use fuels::types::Bytes;
+
+mod success {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn gets_price() {
+        let (_oracle_contract_id, deployer) = setup_environment().await;
+
+        constructor(
+            &deployer.oracle_contract_instance,
+            default_data_sources(),
+            DEFAULT_SINGLE_UPDATE_FEE,
+            EXTENDED_TIME_PERIOD, //As the contract checks against the current timestamp, this allows unit testing with old but real price updates
+            Bytes(guardian_set_upgrade_3_vaa_bytes()),
+        )
+        .await;
+
+        let fee = update_fee(
+            &deployer.oracle_contract_instance,
+            default_update_data_bytes(),
+        )
+        .await
+        .value;
+
+        update_price_feeds(
+            &deployer.oracle_contract_instance,
+            fee,
+            default_update_data_bytes(),
+        )
+        .await;
+
+        let eth_usd_price = price(
+            &deployer.oracle_contract_instance,
+            default_price_feed_ids()[0],
+        )
+        .await
+        .value;
+        let usdc_usd_price = price(
+            &deployer.oracle_contract_instance,
+            default_price_feed_ids()[1],
+        )
+        .await
+        .value;
+
+        assert_eq!(
+            (eth_usd_price.price as f64) * 10f64.powf(-(eth_usd_price.exponent as f64)),
+            ETH_USD_PRICE
+        );
+        assert_eq!(
+            (usdc_usd_price.price as f64) * 10f64.powf(-(usdc_usd_price.exponent as f64)),
+            USDC_USD_PRICE
+        );
+    }
+}
