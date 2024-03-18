55469a1117fdf8650c31eef65e22390ecd62d990
K1-R1 <k1r1.devwork@gmail.com>
2024-03-18 01:56:38 +0000

    Added accumulator update test for update_fee

--- pyth-contract/tests/functions/pyth_core/update_fee.rs
+++ pyth-contract/tests/functions/pyth_core/update_fee.rs
@@ -1,8 +1,9 @@
 use crate::utils::{
     interface::{pyth_core::update_fee, pyth_init::constructor},
     setup::{
-        default_data_sources, default_update_data_bytes, guardian_set_upgrade_3_vaa_bytes,
-        setup_environment, DEFAULT_SINGLE_UPDATE_FEE, DEFAULT_VALID_TIME_PERIOD,
+        default_accumulator_update_data_bytes, default_batch_update_data_bytes,
+        default_data_sources, guardian_set_upgrade_3_vaa_bytes, setup_environment,
+        DEFAULT_SINGLE_UPDATE_FEE, DEFAULT_VALID_TIME_PERIOD,
     },
 };
 use fuels::types::Bytes;
@@ -12,7 +13,7 @@ mod success {
     use super::*;
 
     #[tokio::test]
-    async fn gets_update_fee() {
+    async fn gets_update_fee_for_batch_update() {
         let (_oracle_contract_id, deployer) = setup_environment().await;
 
         constructor(
@@ -26,11 +27,34 @@ mod success {
 
         let fee = update_fee(
             &deployer.oracle_contract_instance,
-            default_update_data_bytes(),
+            default_batch_update_data_bytes(),
         )
         .await
         .value;
 
-        assert_eq!(fee, default_update_data_bytes().len() as u64);
+        assert_eq!(fee, default_batch_update_data_bytes().len() as u64);
+    }
+
+    #[tokio::test]
+    async fn gets_update_fee_for_accumulator_update() {
+        let (_oracle_contract_id, deployer) = setup_environment().await;
+
+        constructor(
+            &deployer.oracle_contract_instance,
+            default_data_sources(),
+            DEFAULT_SINGLE_UPDATE_FEE,
+            DEFAULT_VALID_TIME_PERIOD,
+            Bytes(guardian_set_upgrade_3_vaa_bytes()),
+        )
+        .await;
+
+        let fee = update_fee(
+            &deployer.oracle_contract_instance,
+            default_accumulator_update_data_bytes(),
+        )
+        .await
+        .value;
+
+        assert_eq!(fee, 2);
     }
 }
